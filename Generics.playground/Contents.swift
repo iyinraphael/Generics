69d68c734b274780f6c2914c88913702334211e9
iyinraphael <iyinraphael@gmail.com>
2021-07-17 16:04:11 -0700

    intro to generics

--- Generics.playground/Contents.swift
+++ Generics.playground/Contents.swift
@@ -1,75 +1,32 @@
-//: Playground - noun: a place where people can play
+import Foundation
 
-import UIKit
+// MARK: - Nongeneric function
 
-
-// Create a generic CountedSet struct that is constrained to Hashable elements.
-// A counted set is an unordered collection of unique elements that may appear more than once in the collection.
-// Use a private dictionary as your backing storage for set members and their counts.
-
-struct CountedSet<Element> where Element: Hashable {
-    
-    
-    mutating func insert(_ newElement: Element) {
-        if var count = dictSet[newElement]{
-            count += 1
-            dictSet[newElement] = count
-        }else{
-            dictSet[newElement] = 1
-        }
-        
-    }
-    
-    mutating func remove(_ newElement: Element) {
-        if var count = dictSet[newElement]{
-            count -= 1
-            if count == 0{
-                dictSet.removeValue(forKey: newElement)
-            }else {
-                dictSet[newElement] = count
-            }
-        }
-    }
-    
-    subscript(_ member: Element) -> Int{
-        return dictSet[member] ?? 0
-    }
-    
-    
-    
-    var count: Int {
-        return dictSet.count
-    }
-
-    var isEmpty: Bool {
-        return dictSet.isEmpty
-    }
-    
-    private (set) var dictSet: [Element: Int] = [:]
-    
+func swapTwoInts(_ a: inout Int, b: inout Int) {
+    let tempA = a
+    a = b
+    b = tempA
 }
 
-extension CountedSet: ExpressibleByArrayLiteral{
-    init(arrayLiteral: Element...){
-        for element in arrayLiteral{
-            self.insert(element)
-        }
-    }
+var someInt = 3
+var anotherInt = 107
+swapTwoInts(&someInt, b: &anotherInt)
+print("SomeInt is now \(someInt), and anotherInt is now \(anotherInt)")
+
+
+// MARK: - Generic function
+
+func swapTwoValues<T>(_ a: inout T, b: inout T) {
+    let tempA = a
+    a = b
+    b = tempA
 }
 
-
-enum Arrow { case iron, wooden, elven, dwarvish, magic, silver}
-var aCountedSet = CountedSet<Arrow>()
-aCountedSet[.iron]
-
-var myCountedSet: CountedSet<Arrow> = [.iron, .magic, .iron, .silver, .iron, .iron]
-myCountedSet[.iron]
-myCountedSet.remove(.iron)
-myCountedSet.remove(.dwarvish)
-myCountedSet.remove(.magic)
-
-
-
-
+var someString = "hello"
+var anotherString = "Somebody"
+swapTwoValues(&someString, b: &anotherString)
+swapTwoValues(&someInt, b: &anotherInt)
+print("SomeString is now \(someString), and anotherString is now \(anotherString)")
+print("SomeInt is now \(someInt), and anotherInt is now \(anotherInt)")
 
 
---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Generics
+Generic code enables you to write flexible, resuable functions and types that can work with any type, subject to requirements that you define. YOu can write code that avoids duplication and expresses its intenet in a clear, abstracted manner.
+
+Generics are one of the most powerful features of Swift, and much of the Swift standard library is built with generic code. Generics can also be defined as a placeholder type.
+
+## Where to look
+-   `Generics.swift`
