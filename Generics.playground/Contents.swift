01b5a29a791a94ef988dbf7e85b9459236766f85
iyinraphael <iyinraphael@gmail.com>
2021-07-17 17:44:49 -0700

    generic types

--- Generics.playground/Contents.swift
+++ Generics.playground/Contents.swift
@@ -15,7 +15,7 @@ print("SomeInt is now \(someInt), and anotherInt is now \(anotherInt)")
 
 
 // MARK: - Generic function
-
+/// <T> is a type parameter
 func swapTwoValues<T>(_ a: inout T, b: inout T) {
     let tempA = a
     a = b
@@ -29,4 +29,35 @@ swapTwoValues(&someInt, b: &anotherInt)
 print("SomeString is now \(someString), and anotherString is now \(anotherString)")
 print("SomeInt is now \(someInt), and anotherInt is now \(anotherInt)")
 
+// MARK: - Generic type
 
+struct Stack<Element> {
+    var items: [Element] = []
+    /// These methods are marked as MUTATING, because they need to modify (or mutate) the structure's item array
+    mutating func push(_ item: Element) {
+        items.append(item)
+    }
+    
+    mutating func pop() -> Element {
+        return items.removeLast()
+    }
+}
+
+var stackOfStrings = Stack<String>()
+stackOfStrings.push("uno")
+stackOfStrings.push("dos")
+stackOfStrings.push("tres")
+stackOfStrings.push("cuatro")
+
+let fromTheTop = stackOfStrings.pop()
+print(fromTheTop)
+
+extension Stack {
+    var topItem: Element? {
+        return items.isEmpty ? nil : items[items.count - 1]
+    }
+}
+
+if let topItem = stackOfStrings.topItem {
+    print("The top item on the stack is \(topItem)")
+}
